db7ed2ee330f50b46e82f9ac85553be8443ed6b0
Aldiansyah putra <144991793+aldihostin@users.noreply.github.com>
2024-05-07 09:23:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 function getAnswer() {
     var question = document.getElementById("question").value;
     const answerElement = document.getElementById("answer");
-    const searchButton = document.getElementById("searchButton");
-    const backButton = document.getElementById("backButton");
-
     answerElement.innerText = ''; // Mengosongkan konten jawaban sebelum mengetik jawaban baru
-    searchButton.style.display = "none"; // Menyembunyikan tombol "Cari Jawaban"
-    backButton.style.display = "none"; // Menyembunyikan tombol "Kembali"
-
     fetch('https://aemt.me/gemini?text=' + encodeURIComponent(question.trim()))
     .then(response => {
-        console.log('Response:', response);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         return response.json();
     })
     .then(data => {
-        console.log('Data:', data);
         if (data.status) {
-            answerElement.innerText = data.result; // Menampilkan jawaban atau pesan kesalahan
-            backButton.style.display = "block"; // Menampilkan tombol "Kembali"
+            answerElement.innerText = data.result; // Menampilkan jawaban secara langsung
+            // Memunculkan tombol "Kembali"
+            document.getElementById("backButton").style.display = "block";
         } else {
-            answerElement.innerText = "Maaf, permintaan Anda tentang pertanyaan tersebut tidak dapat kami jawab."; // Menampilkan pesan kesalahan
-            searchButton.style.display = "block"; // Menampilkan tombol "Cari Jawaban"
+            answerElement.innerText = "Maaf, permintaan Anda tentang pertanyaan tersebut tidak dapat kami jawab.";
+            // Menyembunyikan tombol "Kembali" jika jawaban tidak tersedia
+            document.getElementById("backButton").style.display = "none";
         }
     })
     .catch(error => {
         console.error('Error:', error);
-        answerElement.innerText = "Terjadi kesalahan dalam memproses permintaan. Silakan coba lagi nanti."; // Menampilkan pesan kesalahan
-        searchButton.style.display = "block"; // Menampilkan tombol "Cari Jawaban"
     });
-                      }
+}
+
+function resetAnswer() {
+    document.getElementById("answer").innerText = ''; // Menghapus jawaban yang telah ditampilkan
+    document.getElementById("backButton").style.display = "none"; // Menyembunyikan tombol "Kembali"
+}
