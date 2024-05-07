07567d127009040a5ac12747c0f15fdb71927ebc
Aldiansyah putra <144991793+aldihostin@users.noreply.github.com>
2024-05-07 09:13:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function getAnswer() {
     const answerElement = document.getElementById("answer");
     answerElement.innerText = ''; // Mengosongkan konten jawaban sebelum mengetik jawaban baru
     const searchButton = document.getElementById("searchButton");
+    const backButton = document.getElementById("backButton");
     searchButton.style.display = "none"; // Menyembunyikan tombol "Cari Jawaban"
     fetch('https://aemt.me/gemini?text=' + encodeURIComponent(question.trim()))
     .then(response => {
@@ -14,12 +15,14 @@ function getAnswer() {
     .then(data => {
         if (data.status) {
             answerElement.innerText = data.result; // Menampilkan jawaban secara langsung
-            // Memunculkan tombol "Kembali"
-            document.getElementById("backButton").style.display = "block";
+            // Mengganti tombol "Cari Jawaban" dengan tombol "Kembali"
+            searchButton.style.display = "none";
+            backButton.style.display = "block";
         } else {
             answerElement.innerText = "Maaf, permintaan Anda tentang pertanyaan tersebut tidak dapat kami jawab.";
             // Menampilkan kembali tombol "Cari Jawaban" jika jawaban tidak tersedia
             searchButton.style.display = "block";
+            backButton.style.display = "none"; // Menyembunyikan tombol "Kembali"
         }
     })
     .catch(error => {
@@ -32,3 +35,4 @@ function resetAnswer() {
     document.getElementById("backButton").style.display = "none"; // Menyembunyikan tombol "Kembali"
     document.getElementById("searchButton").style.display = "block"; // Menampilkan kembali tombol "Cari Jawaban"
 }
+    
