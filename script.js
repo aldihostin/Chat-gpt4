24a534543a0d666efc40e522ed2b53de95c3a09d
Aldiansyah putra <144991793+aldihostin@users.noreply.github.com>
2024-05-07 09:26:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function getAnswer() {
     var question = document.getElementById("question").value;
     const answerElement = document.getElementById("answer");
     answerElement.innerText = ''; // Mengosongkan konten jawaban sebelum mengetik jawaban baru
+    const searchButton = document.getElementById("searchButton");
+    searchButton.style.display = "none"; // Menyembunyikan tombol "Cari Jawaban"
     fetch('https://aemt.me/gemini?text=' + encodeURIComponent(question.trim()))
     .then(response => {
         if (!response.ok) {
@@ -16,8 +18,8 @@ function getAnswer() {
             document.getElementById("backButton").style.display = "block";
         } else {
             answerElement.innerText = "Maaf, permintaan Anda tentang pertanyaan tersebut tidak dapat kami jawab.";
-            // Menyembunyikan tombol "Kembali" jika jawaban tidak tersedia
-            document.getElementById("backButton").style.display = "none";
+            // Menampilkan kembali tombol "Cari Jawaban" jika jawaban tidak tersedia
+            searchButton.style.display = "block";
         }
     })
     .catch(error => {
@@ -28,4 +30,5 @@ function getAnswer() {
 function resetAnswer() {
     document.getElementById("answer").innerText = ''; // Menghapus jawaban yang telah ditampilkan
     document.getElementById("backButton").style.display = "none"; // Menyembunyikan tombol "Kembali"
+    document.getElementById("searchButton").style.display = "block"; // Menampilkan kembali tombol "Cari Jawaban"
 }
