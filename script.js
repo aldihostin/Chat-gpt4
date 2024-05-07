a4a9a7970adeaed0fa4f2c9ffb64abcc54a26784
Aldiansyah putra <144991793+aldihostin@users.noreply.github.com>
2024-05-07 09:31:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function getAnswer() {
     var question = document.getElementById("question").value;
     const answerElement = document.getElementById("answer");
     answerElement.innerText = ''; // Mengosongkan konten jawaban sebelum mengetik jawaban baru
-    const searchButton = document.getElementById("searchButton");
-    searchButton.style.display = "none"; // Menyembunyikan tombol "Cari Jawaban"
     fetch('https://aemt.me/gemini?text=' + encodeURIComponent(question.trim()))
     .then(response => {
         if (!response.ok) {
@@ -18,8 +16,8 @@ function getAnswer() {
             document.getElementById("backButton").style.display = "block";
         } else {
             answerElement.innerText = "Maaf, permintaan Anda tentang pertanyaan tersebut tidak dapat kami jawab.";
-            // Menampilkan kembali tombol "Cari Jawaban" jika jawaban tidak tersedia
-            searchButton.style.display = "block";
+            // Menyembunyikan tombol "Kembali" jika jawaban tidak tersedia
+            document.getElementById("backButton").style.display = "none";
         }
     })
     .catch(error => {
@@ -30,5 +28,4 @@ function getAnswer() {
 function resetAnswer() {
     document.getElementById("answer").innerText = ''; // Menghapus jawaban yang telah ditampilkan
     document.getElementById("backButton").style.display = "none"; // Menyembunyikan tombol "Kembali"
-    document.getElementById("searchButton").style.display = "block"; // Menampilkan kembali tombol "Cari Jawaban"
-}
+        }
